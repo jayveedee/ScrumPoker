87be3e235c81d0d3d9995ab181dd412168d38631
Jákup Viljam Dam <jayveedees@gmail.com>
2024-01-20 00:03:59 +0000

    made card bouncy with the gyroscope lol

--- app/src/main/java/com/exirpit/scrumpoker/presentation/common/composable/Cards.kt
+++ app/src/main/java/com/exirpit/scrumpoker/presentation/common/composable/Cards.kt
@@ -1,6 +1,10 @@
 package com.exirpit.scrumpoker.presentation.common.composable
 
-import androidx.compose.foundation.Canvas
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,12 +20,17 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
@@ -64,14 +73,44 @@ fun ScrumPokerGridCard(
 fun ScrumPokerExpandedCard(
     modifier: Modifier = Modifier,
     card: Card,
+    usesGyroScope: Boolean = true,
     onExpandedStateChanged: () -> Unit
 ) {
     var isFlipped by remember { mutableStateOf(false) }
 
+    var yAxis by remember { mutableFloatStateOf(0f) }
+
+    if (usesGyroScope) {
+        val sensorManager = LocalContext.current.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val gyroscopeSensor = remember { sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) }
+
+        val sensorEventListener = object : SensorEventListener {
+
+            override fun onSensorChanged(p0: SensorEvent?) {
+                yAxis = if (p0 != null) p0.values[1] else 0f
+            }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+                // Do nothing
+            }
+        }
+
+        DisposableEffect(sensorManager, gyroscopeSensor) {
+            // Register the sensor listener when the Composable is created
+            sensorManager.registerListener(sensorEventListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_GAME)
+
+            onDispose {
+                // Unregister the sensor listener when the Composable is disposed
+                sensorManager.unregisterListener(sensorEventListener)
+            }
+        }
+    }
+
     Card(
         modifier = modifier
             .padding(25.dp)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .graphicsLayer(rotationX = 0f, rotationY = yAxis, rotationZ = 0f),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 10.dp,
             pressedElevation = 5.dp
